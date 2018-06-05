8c1288fc4428613b82ff4a55bdfee0ae59ba6664
Jos Lieben <jos.lieben@ogd.nl>
2018-06-05 16:18:13 +0200

    example comments, hide window and add icon for shortcut

--- intuneServerShareMapper.ps1
+++ intuneServerShareMapper.ps1
@@ -5,11 +5,14 @@
 #Purpose:          Configurable drivemapping to server shares with automatic querying for credentials
 
 #REQUIRED CONFIGURATION
-$driveLetter = "P:"
-$path = "\\server\share"
-$shortCutTitle = "Projects"
+$driveLetter = "U:" #change to desired driveletter
+$path = "\\nlvfs01\dfs-units$\" #change to desired server / share path
+$shortCutTitle = "U-Drive" #this will be the name of the shortcut
 $autosuggestLogin = $True #automatically prefills the login field of the auth popup with the user's O365 email (azure ad join)
-$desiredShortcutLocation = [Environment]::GetFolderPath("Desktop")
+$desiredShortcutLocation = [Environment]::GetFolderPath("Desktop") #you can also use MyDocuments or any other valid input for the GetFolderPath function
+
+
+###START SCRIPT
 
 $desiredMapScriptFolder = Join-Path $Env:LOCALAPPDATA -ChildPath "Lieben.nu"
 $desiredMapScriptPath = Join-Path $desiredMapScriptFolder -ChildPath "SMBdriveMapper.ps1"
@@ -25,22 +28,27 @@ Param(
 )
 "
 if($autosuggestLogin){
-    $scriptContent+= "try{
-        `$objUser = New-Object System.Security.Principal.NTAccount(`$Env:USERNAME)
-        `$strSID = (`$objUser.Translate([System.Security.Principal.SecurityIdentifier])).Value
-        `$basePath = `"HKLM:\SOFTWARE\Microsoft\IdentityStore\Cache\`$strSID\IdentityCache\`$strSID`"
-        if((test-path `$basePath) -eq `$False){
-            `$userId = `$Null
-        }
-        `$userId = (Get-ItemProperty -Path `$basePath -Name UserName).UserName
-    }catch{
-        `$Null
-    }"
+    $scriptContent+= "
+try{
+    `$objUser = New-Object System.Security.Principal.NTAccount(`$Env:USERNAME)
+    `$strSID = (`$objUser.Translate([System.Security.Principal.SecurityIdentifier])).Value
+    `$basePath = `"HKLM:\SOFTWARE\Microsoft\IdentityStore\Cache\`$strSID\IdentityCache\`$strSID`"
+    if((test-path `$basePath) -eq `$False){
+        `$userId = `$Null
+    }
+    `$userId = (Get-ItemProperty -Path `$basePath -Name UserName).UserName
+}catch{
+    `$Null
+}
+"
 }else{
-    $scriptContent+= "`$userId = `$null"
+    $scriptContent+= "
+`$userId = `$null
+    "
 }
 
-$scriptContent+= "`$credentials = Get-Credential -UserName `$userId -Message `"Password required for `$driveLetter`" -ErrorAction Stop
+$scriptContent+= "
+`$credentials = Get-Credential -UserName `$userId -Message `"Password required for `$driveLetter`" -ErrorAction Stop
 [void] [System.Reflection.Assembly]::LoadWithPartialName(`"System.Drawing`") 
 [void] [System.Reflection.Assembly]::LoadWithPartialName(`"System.Windows.Forms`")
 
@@ -58,10 +66,13 @@ try{
     `$OUTPUT= [System.Windows.Forms.MessageBox]::Show(`"Connection failed, technical reason: `$(`$Error[0])`", `"`$driveLetter error`" , 0) 
 }"
 
-$localScriptContent | Out-File $desiredMapScriptPath -Force
+$scriptContent | Out-File $desiredMapScriptPath -Force
 
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut((Join-Path $desiredShortcutLocation -ChildPath "$($shortCutTitle).lnk"))
-$Shortcut.TargetPath = "`"%SystemRoot%\WindowsPowerShell\v1.0\powershell.exe`""
+$Shortcut.TargetPath = "powershell.exe"
+$Shortcut.WorkingDirectory = "%SystemRoot%\WindowsPowerShell\v1.0\"
 $Shortcut.Arguments =  "-WindowStyle Hidden -ExecutionPolicy ByPass -File `"$desiredMapScriptPath`" $driveLetter `"$path`"”
+$Shortcut.IconLocation = "explorer.exe ,0"
+$shortcut.WindowStyle = 7
 $Shortcut.Save()

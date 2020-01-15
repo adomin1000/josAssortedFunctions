8fb0dfbcb983dd5fdfef3cfb4063a7d68c67c876
Jos <jos@lieben.nu>
2020-01-15 09:24:14 +0100

    mini update

--- Enable-BitlockerAndEscrowKeyToAAD.ps1
+++ Enable-BitlockerAndEscrowKeyToAAD.ps1
@@ -9,10 +9,9 @@
 #Name: Enable-BitlockerAndEscrowKeyToAAD.ps1
 
 $logFile = Join-Path $Env:Temp -ChildPath "enable-Bitlocker.log"
-$tenant = "XXXXX.onmicrosoft.com"
-$postKeyToAAD = $True
+$tenant = "tenantname.onmicrosoft.com"
 $ErrorActionPreference = "stop"
-$version = "0.01"
+$version = "0.02"
 $scriptName = "enableBitlocker"
 $scriptPath = $MyInvocation.MyCommand.Definition
 
@@ -25,13 +24,9 @@ try{
     ac $logFile "Failed to schedule task! $_"
 }
 
-try{
-    $bitlockerStatus = Get-BitLockerVolume $env:SystemDrive -ErrorAction Stop | Select-Object -Property VolumeStatus
-}catch{
-    ac $logFile "Failed to retrieve Bitlocker status of system drive $_"
-}
+ac $logFile "Current sysvol status: $((Get-BitLockerVolume $env:SystemDrive -ErrorAction Stop).VolumeStatus)"
 
-if ($bitlockerStatus.VolumeStatus -eq "FullyDecrypted"){
+while ((Get-BitLockerVolume $env:SystemDrive -ErrorAction Stop).VolumeStatus -eq "FullyDecrypted"){
     ac $logFile "$($env:SystemDrive) system volume not yet encrypted, ejecting media and attempting to encrypt"
     try{
         # Automatically unmount any iso/dvd's
@@ -45,7 +40,7 @@ if ($bitlockerStatus.VolumeStatus -eq "FullyDecrypted"){
 
     try{
         # Automatically unmount any USB sticks
-        $volumes = get-wmiobject -Class Win32_Volume | where{$_.drivetype -eq '2'}  
+        $volumes = get-wmiobject -Class Win32_Volume | Where-Object {$_.drivetype -eq '2'}  
         foreach($volume in $volumes){
             $ejectCmd = New-Object -comObject Shell.Application
             $ejectCmd.NameSpace(17).ParseName($volume.driveletter).InvokeVerb("Eject")
@@ -55,55 +50,43 @@ if ($bitlockerStatus.VolumeStatus -eq "FullyDecrypted"){
     }
 
     try{
+        #Enable bitlocker with a normal password protector
+        Enable-BitLocker -MountPoint $env:SystemDrive -UsedSpaceOnly -RecoveryPasswordProtector -ErrorAction Stop -SkipHardwareTest -Confirm:$False
+        ac $logFile "Bitlocker recovery password set"
+    }catch{
+        ac $logFile "Error while setting up AAD Bitlocker password: $_"
+    } 
+
+    try{
         # Enable Bitlocker using TPM
         Enable-BitLocker -MountPoint $env:SystemDrive -UsedSpaceOnly -TpmProtector -ErrorAction Stop -SkipHardwareTest -Confirm:$False
         ac $logFile "Bitlocker enabled using TPM"
     }catch{
         ac $logFile "Failed to enable Bitlocker using TPM: $_"
-        $postKeyToAAD = $False
-        Throw "Error while setting up AAD Bitlocker during TPM step: $_"
     }
-
-    try{
-        #Enable bitlocker with a normal password protector
-        Enable-BitLocker -MountPoint $env:SystemDrive -UsedSpaceOnly -RecoveryPasswordProtector -ErrorAction Stop -SkipHardwareTest -Confirm:$False
-        ac $logFile "Bitlocker recovery password set"
-    }catch{
-        if($_.Exception -like "*0x8031004E*"){
-            ac $logFile "reboot required before bitlocker can be enabled"
-        }else{
-            ac $logFile "Error while setting up AAD Bitlocker: $_"
-            $postKeyToAAD = $False
-            Throw "Error while setting up AAD Bitlocker during noTPM step: $_"
-        }
-    } 
-}else{
-    ac $logFile "System volume $($env:SystemDrive) already encrypted"
 }
 
-if($postKeyToAAD){
-    ac $logFile "Will attempt to update your recovery key in AAD"
+ac $logFile "Will attempt to update your recovery key in AAD"
+try{
+    $cert = dir Cert:\LocalMachine\My\ | Where-Object { $_.Issuer -match "CN=MS-Organization-Access" }
+    $id = $cert.Subject.Replace("CN=","")
+    ac $logFile "using certificate $id"
     try{
-        $cert = dir Cert:\LocalMachine\My\ | where { $_.Issuer -match "CN=MS-Organization-Access" }
-        $id = $cert.Subject.Replace("CN=","")
-        ac $logFile "using certificate $id"
-        try{
-            # Set TLS v1.2
-            $res = [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-            ac $logFile "TLS set to v1.2"
-        }catch{
-            ac $logFile "could not set TLS to v1.2"
-        }
-        (Get-BitLockerVolume -MountPoint $env:SystemDrive).KeyProtector|?{$_.KeyProtectorType -eq 'RecoveryPassword'}|%{
-            $key = $_
-            ac $logFile "kid : $($key.KeyProtectorId) key: $($key.RecoveryPassword)"
-            $body = "{""key"":""$($key.RecoveryPassword)"",""kid"":""$($key.KeyProtectorId.replace('{','').Replace('}',''))"",""vol"":""OSV""}"
-            $url = "https://enterpriseregistration.windows.net/manage/$tenant/device/$($id)?api-version=1.0"
-            $req = Invoke-WebRequest -Uri $url -Body $body -UseBasicParsing -Method Post -UseDefaultCredentials -Certificate $cert
-            ac $logFile "Key updated in AAD"
-        }
-    } catch {
-        ac $logFile "Failed to update key in AAD: $_"
-        Throw "Failed to update key in AAD: $_"
+        # Set TLS v1.2
+        $res = [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        ac $logFile "TLS set to v1.2"
+    }catch{
+        ac $logFile "could not set TLS to v1.2"
     }
+    (Get-BitLockerVolume -MountPoint $env:SystemDrive).KeyProtector | ForEach-Object {$_.KeyProtectorType -eq 'RecoveryPassword'} | ForEach-Object {
+        $key = $_
+        ac $logFile "kid : $($key.KeyProtectorId) key: $($key.RecoveryPassword)"
+        $body = "{""key"":""$($key.RecoveryPassword)"",""kid"":""$($key.KeyProtectorId.replace('{','').Replace('}',''))"",""vol"":""OSV""}"
+        $url = "https://enterpriseregistration.windows.net/manage/$tenant/device/$($id)?api-version=1.0"
+        $req = Invoke-WebRequest -Uri $url -Body $body -UseBasicParsing -Method Post -UseDefaultCredentials -Certificate $cert
+        ac $logFile "Key updated in AAD"
+    }
+} catch {
+    ac $logFile "Failed to update key in AAD: $_"
+    Throw "Failed to update key in AAD: $_"
 }

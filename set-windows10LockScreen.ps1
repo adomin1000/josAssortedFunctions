c9350dcd6a498c733f779df291d46191f1b38fca
Jos <jos@lieben.nu>
2021-05-13 14:21:49 +0200

    switch to user-driven method using w32 api

--- set-windows10LockScreen.ps1
+++ set-windows10LockScreen.ps1
@@ -2,8 +2,7 @@
     .SYNOPSIS
     Sets custom lock screen based on file in an Azure Storage Blob container
     See blob template to automatically configure a blob container: https://gitlab.com/Lieben/assortedFunctions/-/blob/master/ARM%20templates/blob%20storage%20with%20container%20for%20Teams%20Backgrounds%20and%20public%20access.json
-    Also works with Windows 10 Pro / non-enterprise versions
-
+   
     .NOTES
     filename: set-windows10LockScreen.ps1
     author: Jos Lieben
@@ -11,11 +10,11 @@
     created: 13/05/2021
 #>
 
+$changedDate = "2021-05-13"
+$lockscreenFileURL = "https://tasdsadgsadsad.blob.core.windows.net/teamsbackgrounds/figure-a.jpg" #this is the full URL to the desired lock screen image
+
 Start-Transcript -Path (Join-Path -Path $Env:TEMP -ChildPath "set-windows10LockScreen.log")
 
-$changedDate = "2021-05-13"
-
-$lockscreenFileURL = "https://tasdsadgsadsad.blob.core.windows.net/teamsbackgrounds/figure-a.jpg" #this is the name of your storage account in Azure 
 $tempFile = (Join-Path $Env:TEMP -ChildPath "img100.jpg")
 
 try{
@@ -28,33 +27,43 @@ try{
     Exit
 }
 
-#remove buffered lockscreen images
-Write-Output "Deleting image buffers"
-get-childitem "$($env:ProgramData)\Microsoft\Windows\SystemData" -Recurse | % {
-    if($_.Name.StartsWith("LockScreen")){
-        Remove-Item -Path $_.FullName -Force -Confirm:$False -Recurse
-    }
+[Windows.System.UserProfile.LockScreen,Windows.System.UserProfile,ContentType=WindowsRuntime] | Out-Null
+Add-Type -AssemblyName System.Runtime.WindowsRuntime
+
+$asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | ? { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
+Function Await($WinRtTask, $ResultType) {
+    $asTask = $asTaskGeneric.MakeGenericMethod($ResultType)
+    $netTask = $asTask.Invoke($null, @($WinRtTask))
+    $netTask.Wait(-1) | Out-Null
+    $netTask.Result
 }
 
-Start-Process -filePath "$($env:systemRoot)\system32\takeown.exe" -ArgumentList "/F `"$($env:systemRoot)\Web\Screen`" /R /A /D Y" -NoNewWindow -Wait
-Start-Process -filePath "$($env:systemRoot)\system32\icacls.exe" -ArgumentList "`"$($env:systemRoot)\Web\Screen`" /grant Administrators:(OI)(CI)F /T" -NoNewWindow -Wait
-Start-Process -filePath "$($env:systemRoot)\system32\icacls.exe" -ArgumentList "`"$($env:systemRoot)\Web\Screen`" /grant Everyone:(OI)(CI)R /T" -NoNewWindow -Wait
-Start-Process -filePath "$($env:systemRoot)\system32\icacls.exe" -ArgumentList "`"$($env:systemRoot)\Web\Screen`" /reset /T" -NoNewWindow -Wait
-
-Write-Output "Removing current images"
-#remove current lockscreen images
-get-childitem "$($env:systemRoot)\Web\Screen" | % {
-    Remove-Item -Path $_.FullName -Force -Confirm:$False -Recurse
+Function AwaitAction($WinRtAction) {
+    $asTask = ([System.WindowsRuntimeSystemExtensions].GetMethods() | ? { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and !$_.IsGenericMethod })[0]
+    $netTask = $asTask.Invoke($null, @($WinRtAction))
+    $netTask.Wait(-1) | Out-Null
 }
 
-#store new lockscreen image
-Write-Output "Moving $tempFile to destination folder"
-Move-Item -Path $tempFile -Destination "$($env:systemRoot)\Web\Screen\img100.jpg" -Force -Confirm:$False
-
-#Restrict user ability to change lock screen image
-Write-Output "restricting user from modifying the lock screen image"
-New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows" -Name "Personalization" -Force -ErrorAction SilentlyContinue | Out-Null 
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Personalization" -Name 'NoChangingLockScreen' -Value 1 -Type 'Dword' -Force 
+[Windows.Storage.StorageFile,Windows.Storage,ContentType=WindowsRuntime] | Out-Null
+		
+try{
+	$image = Await ([Windows.Storage.StorageFile]::GetFileFromPathAsync($tempFile)) ([Windows.Storage.StorageFile])
+    Write-Output "Image loaded from $tempFile"
+}catch {
+    Write-Output "Failed to load image from $tempFile"
+    Write-Error $_ -ErrorAction SilentlyContinue
+    Exit
+} 
+       
+try{ 
+    Write-Output "Setting image as lock screen image"
+    AwaitAction ([Windows.System.UserProfile.LockScreen]::SetImageFileAsync($image))
+    Write-Output "$tempFile configured as lock screen image"
+    Remove-Item -Path $tempFile -Force -Confirm:$False
+}catch{
+    Write-Output "Failed to set lock screen image"
+    Write-Error $_ -ErrorAction SilentlyContinue
+} 
 
 Write-Output "Script complete"
 Stop-Transcript
